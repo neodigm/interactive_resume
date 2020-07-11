71bc6557b0d0e67c2c85059eb34f7e553a934c96
neodigm <3151842+neodigm@users.noreply.github.com>
2020-07-11 12:07:40 -0500

    doc update

--- spotADify.js
+++ spotADify.js
@@ -1,11 +1,9 @@
-/*
-Install: Open Chrome Dev Tools (Command+option+I on a Mac). Menu > Sources > Snippets
-Install: Create a new Snippet named spotADify.js, Paste this script, Save (Command-S).
+/* Install: Open Chrome Dev Tools (Command+option+I on a Mac). Menu > Sources > Snippets
+   Install: Create a new Snippet named spotADify.js, Paste this script, Save (Command-S).
 
-Usage: Run the Snippet once each time you start the Spotify Web Player.
-Usage: Right-Click the snippet named spotADify.js and choose Run from the drop-down.
-Usage: Close Chrome Dev Tools. 🏖️Play your Jams!
-*/
+   Usage: Run the Snippet once each time you start the Spotify Web Player.
+   Usage: Right-Click the snippet named spotADify.js and choose Run from the drop-down.
+   Usage: Close Chrome Dev Tools. 🏖️Play your Jams! */
 let spotADify = (function(_d, _q, _t){
     let eS = _d.querySelector( _q ), bS = true;
     if( eS ){
