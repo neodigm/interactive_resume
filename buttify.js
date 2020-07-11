c9aebf8bed4d87225a149554ee786a7669f69846
neodigm <3151842+neodigm@users.noreply.github.com>
2020-07-11 00:34:01 -0500

    doc update

---
+++ buttify.js
@@ -0,0 +1,28 @@
+
+🏖️ Inspired Problem Solver 🚀 Visual Storyteller
+
+//  Usage: Save in Chrome > Sources > Snippets
+//  Play your jams
+//  Run the Snippet. Done.
+
+var buttify = (function(_d, _q, _t){
+  var eS = _d.querySelector( _q ), bS = true;
+  if( eS ){
+  setInterval(function(){buttify.tick();}, _t);
+  return {
+      "tick": function(){
+        if( (_d.title.indexOf("Adv") != -1) || (_d.title.indexOf("Spot") != -1) ){
+            if( bS ){
+                eS.click();
+                bS=false;
+            }
+        }else{
+            if( !bS ){
+                eS.click();
+                bS=true;
+            }
+        }
+      }
+    }
+  }
+})( document, "[aria-label='Mute'],[aria-label='Unmute']", 220);
