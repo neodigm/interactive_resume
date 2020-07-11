0bdfca1dece6863517bd4450a490abeb31319719
neodigm <3151842+neodigm@users.noreply.github.com>
2020-07-11 00:41:19 -0500

    doc update

--- buttify.js
+++ buttify.js
@@ -3,22 +3,22 @@
 
 
 
-//  Usage: Save in Chrome > Sources > Snippets
+//  Usage: Save in Chrome Dev Tools > Sources > Snippets
+//  Run the Snippet once each time you start the Spotify Web Player
 //  Play your jams
-//  Run the Snippet each time you use the Spotify Web Player
 
-var buttify = (function(_d, _q, _t){
-  var eS = _d.querySelector( _q ), bS = true;
-  if( eS ){
-  setInterval(function(){buttify.tick();}, _t);
-  return {
-      "tick": function(){
-        if( (_d.title.indexOf("Adv") != -1) || (_d.title.indexOf("Spot") != -1) ){
-            if( bS ){ eS.click(); bS=false; }
-        }else{
-            if( !bS ){ eS.click(); bS=true; }
+let buttify = (function(_d, _q, _t){
+    let eS = _d.querySelector( _q ), bS = true;
+    if( eS ){
+    setInterval(function(){buttify.tick();}, _t);
+    return {
+        "tick": function(){
+          if( (_d.title.indexOf("Adv") != -1) || (_d.title.indexOf("Spot") != -1) ){
+              if( bS ){ eS.click(); bS=false; }
+          }else{
+              if( !bS ){ eS.click(); bS=true; }
+          }
         }
       }
     }
-  }
-})( document, "[aria-label='Mute'],[aria-label='Unmute']", 220);
+  })( document, "[aria-label='Mute'],[aria-label='Unmute']", 220);
