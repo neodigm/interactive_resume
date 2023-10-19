6ad840e2cb7b2281c0c9e1c17ac790c2bc8d9184
neodigm <3151842+neodigm@users.noreply.github.com>
2023-10-19 17:29:50 -0500

    Neodigm_interactive

---
+++ fuckyousw.js
@@ -0,0 +1,36 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js'
+);
+
+console.log( "workbox 6.5.4 | 20221014 (5s timeout)", workbox )
+const {registerRoute} = workbox.routing;
+const {NetworkFirst} = workbox.strategies;
+//  const {CacheableResponsePlugin} = workbox.cacheable.response;
+
+workbox.LOG_LEVEL = "debug";
+try {
+  self.skipWaiting()
+  clients.claim();
+} catch (error) {
+  console.log("---- claim error | ", error )
+}
+
+const cacheName = 'Neodigm55Cache';
+//  const matchCallback = ({request}) => request.mode === 'navigate';
+const matchCallback = ({request}) => {
+  //if(request.url.indexOf(".vue") != -1) console.log("---- sw req | ", request.url)
+  return true;
+};
+
+const networkTimeoutSeconds = 5;
+
+registerRoute(
+  matchCallback,
+  new NetworkFirst({
+    networkTimeoutSeconds,
+    "cacheName": "Neodigm55Cache",
+    "matchOptions": {"ingoreSearch": true}
+  })
+);
+
+console.log("Arcanus 55 ~ Neodigm 55 ~ SW v2.3 | " + workbox.LOG_LEVEL );
